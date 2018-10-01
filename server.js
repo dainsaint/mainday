471ada7da9f6ba1ad6f2498dea0a309f06306fbc
Dain Saint <dain@cipherprime.com>
2018-10-01 14:46:23 -0400

    reconfigure

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 var http = require("http");
+var parser = require("./parser");
 
-
-function start( externalRequest )
+function start()
 {
     function onRequest( request, response )
     {
         console.log("request received");
+        new Promise( parser.getCalendar )
+            .then( writeResponse.partial(response) );
+    }
+
+    function writeResponse( response, content )
+    {
         response.writeHead(200, {"Content-Type": "text/plain"});
-        // response.write("Hello World");
-        externalRequest( response );
-        // response.end();
+        response.write( content )
+        response.end();
     }
 
     http.createServer( onRequest ).listen(443);
     console.log("server started");
 }
 
-exports.start = start;
+start();
+
+// exports.start = start;
